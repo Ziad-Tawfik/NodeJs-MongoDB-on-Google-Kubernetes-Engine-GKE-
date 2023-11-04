4ef541282c1c8416826b4720a491de6d66857985
Zyad Tawfik <ziad.muhammed.othman@gmail.com>
2023-11-04 20:56:06 +0200

    fix jenkins vm script

--- Jenkins-Infra-Terraform/jenkins_vm_script.sh
+++ Jenkins-Infra-Terraform/jenkins_vm_script.sh
@@ -3,12 +3,20 @@
 # Update the system
 sudo apt-get upgrade && sudo apt-get update -y
 
+# Install dependencies
+sudo apt-get install -y apt-transport-https ca-certificates gnupg \
+ curl sudo unzip software-properties-common gnupg2 wget
+
 # Install git
 sudo apt-get install -y git
 
+# Install Terraform
+sudo wget https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_linux_amd64.zip
+sudo unzip terraform_1.5.7_linux_amd64.zip -d /usr/local/bin/ 
+sudo rm terraform_1.5.7_linux_amd64.zip
+
 # Install Java
 sudo apt install -y openjdk-17-jre-headless
-
 
 # Install Jenkins
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
@@ -19,13 +27,7 @@ sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
 sudo apt-get update -y
 sudo apt-get install -y jenkins
 
-# Install Terraform
-sudo wget https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_linux_amd64.zip \
-  && sudo unzip terraform_1.5.7_linux_amd64.zip -d /usr/local/bin/  \
-  && sudo rm terraform_1.5.7_linux_amd64.zip
-
 # Install gcloud components
-sudo apt-get install -y apt-transport-https ca-certificates gnupg curl sudo unzip software-properties-common gnupg2 wget
 sudo echo "deb [signed-by=/usr/share/keyrings/cloud.google.asc] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.asc
 sudo apt-get update -y
